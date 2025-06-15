35acee95d3eb3fa4d7f39f9f5e198901236604dd
Rafael Mata <83139503+Rafa-Mt@users.noreply.github.com>
2025-06-15 17:15:16 -0400

    added form validation and started auth routes

--- backend_server/routes/auth.py
+++ backend_server/routes/auth.py
@@ -1,14 +1,25 @@
-from flask import Blueprint, request, jsonify
+from flask import Blueprint, request
+from flask_pydantic import validate
 from services.redis import session_storage
+from services.body_schemas import LoginSchema, RegisterSchema, SendResetTokenSchema, ResetPasswordSchema
 
 auth = Blueprint('auth', __name__, url_prefix='/auth')
-@auth.post("/redis")
-def load_to_redis():
-    body = request.get_json()
+
+@auth.post("/login")
+@validate(body = LoginSchema)
+def login(body: LoginSchema):
+
     print(body)
     return body
 
-@auth.get('/')
-def hello_world():
+@auth.get('/register')
+@validate()
+def register():
     value = session_storage.hgetall("object_test")
     return {'message': 'Hello, World!', 'value': value}
+
+def send_reset_token():
+    ...
+
+def reset_password():
+    ...
