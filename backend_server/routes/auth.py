49d3ca874f02891d2485aac568a10d50c3c0cfc6
Rafael Mata <83139503+Rafa-Mt@users.noreply.github.com>
2025-06-16 00:17:15 -0400

    started all auth logic, register implemented

--- backend_server/routes/auth.py
+++ backend_server/routes/auth.py
@@ -1,22 +1,45 @@
 from flask import Blueprint, request
 from flask_pydantic import validate
+from flask_bcrypt import Bcrypt
 from services.redis import session_storage
-from services.body_schemas import LoginSchema, RegisterSchema, SendResetTokenSchema, ResetPasswordSchema
+from services.schema_validation import LoginSchema, RegisterSchema, SendResetTokenSchema, ResetPasswordSchema
+from services.db import UserResponse, create_user, fetch_user_by_username
 
 auth = Blueprint('auth', __name__, url_prefix='/auth')
 
+bcrypt = Bcrypt()
+
 @auth.post("/login")
-@validate(body = LoginSchema)
-def login(body: LoginSchema):
-
-    print(body)
-    return body
-
-@auth.get('/register')
 @validate()
-def register():
-    value = session_storage.hgetall("object_test")
-    return {'message': 'Hello, World!', 'value': value}
+def login(body: LoginSchema):
+    retrieved_user: UserResponse = fetch_user_by_username(username=body.username)
+    if retrieved_user is None:
+        raise KeyError("User does not exist")
+    
+    return {
+        "message": "Found user",
+        "data": retrieved_user.model_dump(exclude=["password",  "id"])
+    }
+
+@auth.post('/register')
+@validate()
+def register(body: RegisterSchema):
+    user_exists = fetch_user_by_username(username=body.username) is not None
+    if user_exists:
+        raise ValueError("User already exist")
+    
+    hashed_password = bcrypt.generate_password_hash(body.password, 8) \
+        .decode('utf-8')
+    
+    create_user(
+        username=body.username,
+        email=body.email,
+        password=hashed_password
+    )
+
+    return {
+        "message": "User created successfully"
+    }
 
 def send_reset_token():
     ...
--- backend_server/services/db.py
+++ backend_server/services/db.py
@@ -1,6 +1,75 @@
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, Table,  MetaData, engine, select
+from sqlalchemy.exc import IntegrityError, OperationalError
 from os import getenv
+from functools import wraps
+from uuid import UUID
+from pydantic import BaseModel
 
 pg = create_engine(
     f"postgresql://{getenv('POSTGRES_USER')}:{getenv('POSTGRES_PASSWORD')}@{getenv('POSTGRES_HOST')}:{getenv('POSTGRES_PORT')}/{getenv('POSTGRES_DB')}"
 )
+metadata = MetaData()
+
+users = Table('user', metadata, autoload_with=pg)
+posts = Table('post', metadata, autoload_with=pg)
+
+class DatabaseError(Exception):
+    pass
+
+Connection = engine.Connection
+
+def query(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        try:
+            with pg.connect() as conn:
+                with conn.begin():
+                    return func(conn, *args, **kwargs)
+                
+        except IntegrityError as e:
+            raise DatabaseError(f"Database constraint violated: {e}")
+        except OperationalError as e:
+            raise DatabaseError(f"Database connection failed: {e}")
+        except Exception as e:
+            raise DatabaseError(f"Unexpected database error: {e}")
+
+    return wrapper
+
+def database_error_handler(error):
+    ...
+
+@query
+def create_user(conn: Connection | None, username: str, password: str, email: str):
+    return conn.execute(
+        users.insert() \
+        .values(username=username, password=password, email=email)
+    )
+
+class UserResponse(BaseModel):
+    id: UUID
+    username: str
+    password: str
+    email: str
+
+@query
+def fetch_users(conn: Connection | None) -> list[UserResponse | None]:
+    users = conn.execute(
+        select(*users.c)
+    ).fetchall()
+    return [UserResponse.model_validate(row._asdict()) if row else None for row in users]
+
+@query
+def fetch_user_by_id(conn: Connection | None, user_id: UUID) -> UserResponse | None:
+    row =  conn.execute(
+        select(users.c.id, users.c.username, users.c.email) \
+        .where(users.c.id == user_id)
+    ).fetchone()
+    return UserResponse.model_validate(row._asdict()) if row else None
+
+@query
+def fetch_user_by_username(conn: Connection | None, username: str) -> UserResponse | None:
+    row = conn.execute(
+        select(users.c.id, users.c.username, users.c.email, users.c.password) \
+        .where(users.c.username == username) \
+    ).fetchone()
+    return UserResponse.model_validate(row._asdict()) if row else None
