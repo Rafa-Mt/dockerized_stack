4453dc584022150f2e6c5dc31597cbea658e1211
Rafael Mata <83139503+Rafa-Mt@users.noreply.github.com>
2025-06-16 00:13:58 -0400

    added error handlers

--- backend_server/main.py
+++ backend_server/main.py
@@ -3,13 +3,22 @@ from dotenv import load_dotenv
 from os import getenv
 from routes.auth import auth
 from routes.posts import posts
+from services.schema_validation import ValidatorError, validation_handler, custom_error_handler
 
 load_dotenv()  
-
 app = Flask(__name__)
 
 if __name__ == '__main__':
     enviroment = getenv('ENVIRONMENT', 'development')
+
+    @app.errorhandler(ValidatorError)
+    def validator_wrapper(error):
+        return validation_handler(error)
+    
+    @app.errorhandler(Exception)
+    def exception_handler(error):
+        return custom_error_handler(error)
+    
     app.register_blueprint(auth)
     app.register_blueprint(posts)
     app.run(
